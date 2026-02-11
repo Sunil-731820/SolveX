2382606c06dba86e824df44e8ef37cf8ea3b10d8
Sunil-731820 <sunilg@infinite.com>
2026-02-11 14:54:00 +0530

    same project added to main

--- src/main/java/com/java/real/SolveXApplication.java
+++ src/main/java/com/java/real/SolveXApplication.java
@@ -2,13 +2,22 @@ package com.java.real;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SolveXApplication {
 
 	public static void main(String[] args) {
 		System.out.println("This is Projects ");
 		SpringApplication.run(SolveXApplication.class, args);
+	}
+	
+//	Adding one end point for Our Projects 
+	@GetMapping("/hello")
+	public String helloEndPoint() {
+		return "The project is started called as SolveX";
 	}
 
 }
