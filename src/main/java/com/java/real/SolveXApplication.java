6be9973497180771acae17ae7f25a3b6fa1a189a
Sunil-731820 <sunilg@infinite.com>
2026-02-11 13:01:29 +0530

    logger added

--- src/main/java/com/java/real/SolveXApplication.java
+++ src/main/java/com/java/real/SolveXApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SolveXApplication {
 
 	public static void main(String[] args) {
+		System.out.println("This is Projects ");
 		SpringApplication.run(SolveXApplication.class, args);
 	}
 
