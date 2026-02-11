7203978a18eecb7d21330c10f58b97cbbd407134
Sunil-731820 <sunilg@infinite.com>
2026-02-11 12:58:40 +0530

    project started

---
+++ src/main/java/com/java/real/SolveXApplication.java
@@ -0,0 +1,13 @@
+package com.java.real;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolveXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolveXApplication.class, args);
+	}
+
+}
